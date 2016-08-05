1d120f64a340254bb5035601989513f008b0494b
qwang51 <qianranw@gmail.com>
2016-08-04 19:14:39 -0700

    Add error handling and warning to repo

--- requester.py
+++ requester.py
@@ -1,6 +1,8 @@
 import urllib2
 import pandas as pd
+import warnings
 import os
+import re
 import csv
 
 
@@ -13,18 +15,22 @@ def url_to_csv(url, fname):
     """
     try:
         connection = urllib2.urlopen(url)
+    except IOError:
+        raise ValueError, 'Invalid url'
+    else:
         data = connection.read()
-        with open(fname, 'wb') as csvFile:
-            csvFile.write(data)
-        with open(fname, 'r') as f:
-            content = csv.reader(f)
-            for row in content:
-                if len(row) != 0:
-                    continue
-                else:
-                    raise TypeError
-    except urllib2.URLError, e:
-        return e.args
+        with open(fname, 'wb') as file:
+            file.write(data)
+        with open(fname, 'r') as csvfile:
+            dialect = csv.Sniffer().sniff(csvfile.read())
+            csvfile.seek(0)
+            delimiter = dialect.delimiter
+            rows = [line.split(delimiter) for line in csvfile.readlines()]
+            if all([len(rows[0])==len(elem) for elem in rows]):
+                return
+            else:
+                os.remove(os.path.join(os.getcwd(),fname))
+                raise TypeError, 'Not valid csv format'
 
 
 def batch_url_to_csv(urls, fnames):
@@ -40,8 +46,28 @@ def batch_url_to_csv(urls, fnames):
         i = 0
         paths = []
         while i < len(urls):
-            url_to_csv(urls[i], fnames[i])
-            paths.append(os.path.join(os.getcwd(),fnames[i]))
+            try:
+                connection = urllib2.urlopen(urls[i])
+            except:
+                warnings.warn('Invalid url', RuntimeWarning)
+                pass
+            else:
+                data = connection.read()
+                with open(fnames[i], 'wb') as csvfile:
+                    csvfile.write(data)
+                paths.append(os.path.join(os.getcwd(),fnames[i]))
+                with open(fnames[i], 'r') as f:
+                    dialect = csv.Sniffer().sniff(f.read())
+                    f.seek(0)
+                    delimiter = dialect.delimiter
+                    rows = [line.split(delimiter) for line in f.readlines()]
+                    if all([len(rows[0])==len(elem) for elem in rows]):
+                        pass
+                    else:
+                        warnings.warn('File %d is not CSV format' % i, RuntimeWarning)
+                        os.remove(os.path.join(os.getcwd(),fnames[i]))
+                        paths.remove(os.path.join(os.getcwd(),fnames[i]))
+                        pass
             i += 1
         return paths
     else:
@@ -61,4 +87,66 @@ def url_to_df(url):
     return df
 
 
-# url = "https://archive.ics.uci.edu/ml/machine-learning-databases/abalone/abalone.data"
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/abalone/abalone.data"
+urls = ['https://archive.ics.uci.edu/ml/machine-learning-databases/audiology/audiology.data',
+       'https://archive.ics.uci.edu/ml/machine-learning-databases/abalone/abalone.data',
+        'http://www.h.com']
+fnames = ['c1.csv','c2.csv','c3.csv']
+wrongurl1 = 'http://www.google.com'
+wrongurl2 = 'https://archive.ics.uci.edu/ml/machine-learning-databases/audiology/audiology.data'
+wrongurl3 = 'hi'
+fname = 'a.csv'
+commaURL = 'https://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/reprocessed.hungarian.data'
+
+# url_to_csv(wrongurl2, fname)
+
+
+# for u in urls:
+#     try:
+#         connection = urllib2.urlopen(u)
+#     except:
+#         warnings.warn('Invalid', RuntimeWarning)
+#         pass
+#     else:
+#         print connection
+# if IOError:
+#     warnings.warn('Invalid URL', RuntimeWarning)
+# else:
+#     data = connection.read()
+# print batch_url_to_csv(urls, fnames)
+# url_to_csv(url, fname)
+
+# with open('edge.csv', 'r') as csvfile:
+#     s = csv.Sniffer()
+#     d = s.sniff(csvfile.read())
+#     csvfile.seek(0)
+#     print d.delimiter
+#     content = csv.reader(csvfile)
+#     c = [line.split(d.delimiter) for line in csvfile.readlines()]
+#     for row in c:print row
+#     # for i in c[1:]:
+#     #     if len(i) > len(c[0]):
+#     #         print("found a data row that is longer then the header row.")
+#     # blank = re.compile(r'\s*')
+#     # for i in c[0]:
+#     #     if blank.match(i).end() == len(i):
+#     #         print("found an empty header item")
+#     l = all([len(c[0])==len(row) for row in c])
+#     # print l
+#     if l is True:
+#         charset = re.compile(r'\w*$')
+#         special = True
+#         for row in c:
+#             # for elem in row:
+#             #     if re.match(charset, elem):
+#             #         # print elem
+#             #         continue
+#             #     else:
+#             #         print elem, "has special character"
+#             special = all([re.match(charset, elem) for elem in row])
+#                 # if not re.match(charset, elem):
+#                 #     print "Contains special character"
+#                 #     break
+#                 # else: print "Valid csv"
+#         print special, 'specila'
+#     else: print "Length not equal"
--- test.py
+++ test.py
@@ -1,24 +1,37 @@
 import unittest
 import os
+import warnings
 import requester
 
 
 class TestStringMethods(unittest.TestCase):
 
-    csvUrls = []
-    urls = ['http://stackoverflow.com/questions/19557801/how-to-make-a-function-that-check-if-the-csv-file-is-valid-or-not-python']
-    wrongUrls = ['a','b','hi']
+    csv_urls = ['https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data',
+               'https://archive.ics.uci.edu/ml/machine-learning-databases/hayes-roth/hayes-roth.data',
+               'https://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/processed.va.data',
+               'https://archive.ics.uci.edu/ml/machine-learning-databases/housing/housing.data',
+               'https://archive.ics.uci.edu/ml/machine-learning-databases/glass/glass.data',
+               'https://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/reprocessed.hungarian.data']
+    invalid_csv_urls = ['http://stackoverflow.com/questions/19557801/how-to-make-a-function-that-check-if-the-csv-file-is-valid-or-not-python',
+                        'https://archive.ics.uci.edu/ml/machine-learning-databases/audiology/audiology.data']
+    wrongUrls = ['hi','www.google','www.apple.com']
     fnames = ['f','g','h']
 
-    def test_valid_url(self):
+
+    def assertValueError(self):
         with self.assertRaises(ValueError):
             for url in self.wrongUrls:
                 requester.url_to_csv(url, self.fnames[0])
 
-    def test_csv_url(self):
+    def assertTypeError(self):
         with self.assertRaises(TypeError):
-            for url in self.urls:
+            for url in self.invalid_csv_urls:
                 requester.url_to_csv(url, self.fnames[0])
+
+    def assertWarnings(self):
+        with self.assertRaises(warnings.warn(RuntimeWarning)):
+            for url in self.wrongUrls:
+                requester.batch_url_to_csv(url, self.fnames[0])
 
 
 
